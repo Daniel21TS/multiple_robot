749ae28c3cfc475a44c20ca6aa87287f0e8a7aa1
Daniel21TS <116293297+Daniel21TS@users.noreply.github.com>
2023-12-13 19:44:34 +0000

    Add files via upload

---
+++ VALUES.py
@@ -0,0 +1,21 @@
+3.5, Map Type=1, Norm information Gain=5.345138888390336, Frontier Density Ponderada=0.12845036111921224
+1.05, Map Type=1, Norm information Gain=-9.182552175978865, Frontier Density Ponderada=0.21577974715443737
+0.75, Map Type=1, Norm information Gain=39.55067134495559, Frontier Density Ponderada=0.3698860786702013
+0.35000000000000003, Map Type=1, Norm information Gain=69.2357349590332, Frontier Density Ponderada=1.0614233513137288
+x=-24.125000359490514, y=-36.6000005453825, Frontier Size=8.15, Map Type=1, Norm information Gain=98.56591497697602, Frontier Density Ponderada=0.0367284146601936
+x=-28.525000425055623, y=-36.15000053867698, Frontier Size=0.45, Map Type=1, Norm information Gain=236.7329926428751, Frontier Density Ponderada=0.31320689055159207
+x=-22.625000337138772, y=-37.10000055283308, Frontier Size=11.8, Map Type=1, Norm information Gain=115.05037712706586, Frontier Density Ponderada=0.021673712115784192
+x=-28.725000428035855, y=-36.55000054463744, Frontier Size=0.25, Map Type=1, Norm information Gain=90.4773231253161, Frontier Density Ponderada=0.02773846449723108
+x=-24.375000363215804, y=-37.65000056102872, Frontier Size=8.8, Map Type=1, Norm information Gain=86.95013084706652, Frontier Density Ponderada=0.4684718448421249
+x=-28.525000425055623, y=-37.10000055283308, Frontier Size=1.2000000000000002, Map Type=1, Norm information Gain=207.12577212759894, Frontier Density Ponderada=0.014273751522533492
+x=-30.17500044964254, y=-33.100000493228436, Frontier Size=8.15, Map Type=1, Norm information Gain=200.95753058924208, Frontier Density Ponderada=0.887419523229511
+x=-29.175000434741378, y=-37.40000055730343, Frontier Size=0.5, Map Type=1, Norm information Gain=72.8047999575834, Frontier Density Ponderada=0.8787193318252999
+x=-21.37500031851232, y=-38.90000057965517, Frontier Size=5.45, Map Type=1, Norm information Gain=56.04557108433385, Frontier Density Ponderada=0.6633895945710804
+x=-25.325000377371907, y=-37.85000056400895, Frontier Size=9.8, Map Type=1, Norm information Gain=896.645817378802, Frontier Density Ponderada=0.2362233787325139
+x=-23.575000351294875, y=-39.95000059530139, Frontier Size=1.1, Map Type=1, Norm information Gain=1996.4965541685733, Frontier Density Ponderada=0.11799634591961022
+x=-29.275000436231494, y=-40.300000600516796, Frontier Size=0.5, Map Type=1, Norm information Gain=204.85631809927457, Frontier Density Ponderada=0.017177300977544583
+x=-18.875000281259418, y=-38.70000057667494, Frontier Size=5.45, Map Type=1, Norm information Gain=886.8671599383899, Frontier Density Ponderada=0.11894792935444579
+x=-22.775000339373946, y=-40.40000060200691, Frontier Size=0.35000000000000003, Map Type=1, Norm information Gain=88.39704465728055, Frontier Density Ponderada=0.37845832608317376
+x=-17.875000266358256, y=-39.550000589340925, Frontier Size=9.0, Map Type=1, Norm information Gain=24.996153789938298, Frontier Density Ponderada=0.2740560292326431
+x=-19.32500028796494, y=-36.25000054016709, Frontier Size=0.55, Map Type=1, Norm information Gain=1142.6345488078161, Frontier Density Ponderada=0.22007529620197097
+x=-15.575000232085586, y=-41.350000616163015, Frontier Size=0.2, Map Type=1, Norm information Gain=214.45173862174022, Frontier Density Ponderada=0.08586090533335994
